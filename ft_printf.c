baf3ee08e83ab399a9262540587cee282c6a6c9c
HugoMonVaz <hmontoyavazquez@gmail.com>
2023-12-10 18:54:08 +0100

    Path to header corrected

--- ft_printf.c
+++ ft_printf.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/ft_printf.h"
+#include "ft_printf.h"
 
 int	ft_is_strformat(const char first, const char second)
 {
